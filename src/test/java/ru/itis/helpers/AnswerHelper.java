1f5805cbc4487331ffcbc6cf96008908d127c7a2
Lesya <sikamikanico9494@gmail.com>
2019-11-23 14:25:49 +0300

    Missing helper added)))0)

---
+++ src/test/java/ru/itis/helpers/AnswerHelper.java
@@ -0,0 +1,22 @@
+package ru.itis.helpers;
+
+import org.openqa.selenium.By;
+import ru.itis.AppManager;
+import ru.itis.models.Answer;
+
+public class AnswerHelper extends BaseHelper {
+
+    public AnswerHelper(AppManager appManager) {
+        super(appManager);
+    }
+
+
+    public void editAnswer(Answer newAnswer) {
+        driver.get("http://localhost:8080/profile/lesya");
+        driver.findElement(By.id("edit-tab")).click();
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div/input")).click();
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div/input")).clear();
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div/input")).sendKeys(newAnswer.getText());
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div/div/button")).click();
+    }
+}
